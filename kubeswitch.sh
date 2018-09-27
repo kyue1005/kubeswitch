468d1e58018b1ab01c38e27a20102af89811c05e
Kelvin Yue <kelvin.yue@scmp.com>
2018-09-27 12:27:20 +0800

    feat: Dowload from google storage if version not found

--- kubeswitch.sh
+++ kubeswitch.sh
@@ -14,6 +14,14 @@ VERSION=$1
 
 if [ ! -f $KUBECTL_DIR/kubectl-$VERSION ]; then
     echo "File not found!"
+    echo "Trying to download from google storage..."
+    curl -Lo $KUBECTL_DIR/kubectl-$VERSION https://storage.googleapis.com/kubernetes-release/release/v$VERSION/bin/darwin/amd64/kubectl
+    if [ -f $KUBECTL_DIR/kubectl-$VERSION ]; then
+        chmod +x $KUBECTL_DIR/kubectl-$VERSION
+    else
+        echo "Version not exists"
+        exit 1;
+    fi
 fi
 
 rm $KUBECTL_BIN && ln -s $KUBECTL_DIR/kubectl-$VERSION $KUBECTL_BIN
